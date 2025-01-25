5ad9a405bf6b53e6b2a0ec9ee5a581b71b44b59b
lyle2000 <1345869472@qq.com>
2025-01-25 22:08:54 +0800

    使用OpenFeign

--- cart-service/src/main/java/com/hmall/cart/service/Impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/Impl/CartServiceImpl.java
@@ -1,8 +1,11 @@
 package com.hmall.cart.service.Impl;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.cart.client.ItemClient;
 import com.hmall.cart.domain.dto.CartFormDTO;
 import com.hmall.cart.domain.dto.ItemDTO;
 import com.hmall.cart.domain.po.Cart;
@@ -14,6 +17,8 @@ import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -39,9 +44,13 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
 
-    private final RestTemplate restTemplate;
+//    private final RestTemplate restTemplate;
+//
+//    private final DiscoveryClient discoveryClient;
 
-    private String name = "";
+    private final ItemClient itemClient;
+
+    //private String name = "";   // TODO
 
 
     @Override
@@ -89,22 +98,31 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         // TODO 1.获取商品id
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
         // 2.查询商品
-        //List<ItemDTO> items = itemService.queryItemByIds(itemIds);
-        //2.1. 利用RestTemplate发起http请求，得到http的响应
-        ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
-                "http://localhost:8081/items?ids={ids}",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<ItemDTO>>() {},
-                Map.of("ids", CollUtils.join(itemIds, ","))
-        );
-        //2.2.解析响应
-        if(!response.getStatusCode().is2xxSuccessful()){
-            // 查询失败
-            return;
-        }
-        List<ItemDTO> items = response.getBody();
+//        //2.1.根据服务名称获取服务的实例列表
+//        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+//        if(CollUtils.isEmpty(instances)) {
+//            return;
+//        }
+//        //2.2.手写负载均衡，从实例列表中挑选一个实例
+//        ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
+//        // List<ItemDTO> items = itemService.queryItemByIds(itemIds);
+//        // 2.3.利用RestTemplate发起http请求，得到http的响应
+//        ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
+//                instance.getUri() + "/items?ids={ids}",
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<List<ItemDTO>>() {
+//                },
+//                Map.of("ids", CollUtil.join(itemIds, ","))
+//        );
+//        // 2.2.解析响应
+//        if(!response.getStatusCode().is2xxSuccessful()){
+//            // 查询失败，直接结束
+//            return;
+//        }
+//        List<ItemDTO> items = response.getBody();
 
+         List<ItemDTO> items =itemClient.queryItemByIds(itemIds);
         if (CollUtils.isEmpty(items)) {
             return;
         }
